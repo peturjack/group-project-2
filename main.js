27795a3a2687d7a4bfb20455d4b635c34f3d34fe
rebekkarutm <rmdottir@gmail.com>
2023-10-10 16:02:01 +0000

    JS files tidied up

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ setInterval(function () {
     rem -= 7.3;
     logoAnimation.style.marginTop = rem + "rem";
 }, 150);
+//sheep animation
+let id = 0;
+function mySheep() {
+    let sheep = document.querySelector(".sheep img");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 0.2);
+    function frame() {
+        if (pos >= 390) {
+            clearInterval(id);
+        }
+        else {
+            if (sheep === null) {
+                clearInterval(id);
+                return;
+            }
+            pos++;
+            sheep.style.position = "absolute";
+            sheep.style.left = pos + "px";
+        }
+    }
+}
--- main.ts
+++ main.ts
@@ -6,3 +6,26 @@ setInterval(function() {
     rem -= 7.3
     logoAnimation.style.marginTop = rem + "rem"
 }, 150)
+
+//sheep animation
+let id: number = 0;
+
+function mySheep() {
+    let sheep: HTMLImageElement | null = document.querySelector(".sheep img");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 0.2);
+    function frame(){
+        if (pos >= 390) {
+            clearInterval(id);
+        } else {
+            if (sheep === null) {
+                clearInterval(id);
+                return;
+            }
+            pos++;
+            sheep.style.position = "absolute";
+            sheep.style.left = pos + "px";
+        }
+    }
+}
