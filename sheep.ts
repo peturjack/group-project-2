ea9550abf313eca45e3c00a668aadbc5ed8ac199
rebekkarutm <rmdottir@gmail.com>
2023-10-11 14:43:29 +0000

    files tidied up

--- sheep.ts
+++ sheep.ts
@@ -1,12 +1,3 @@
-//logo animation
-let rem: number = 0;
-let logoAnimation: HTMLImageElement | null = document.querySelector(".logoAnimation img");
-setInterval(function() {
-    if (logoAnimation === null || -7.3*7 >= rem) {return}
-    rem -= 7.3
-    logoAnimation.style.marginTop = rem + "rem"
-}, 150)
-
 //sheep animation
 let id: number = 0;
 function mySheep() {
