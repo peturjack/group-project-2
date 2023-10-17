a072ccc39c4ec286d594eaf669b3fa7684a00c5c
Petur jack <peturwj@gmail.com>
2023-10-17 09:21:48 +0000

    added the javascript search filter

---
+++ TypeScript/filter.ts
@@ -0,0 +1,21 @@
+function dropDown() {
+    const filterDropdown = document.getElementById("filter-dropdown");
+    const filterIcon = document.getElementById("filter-icon");
+    const container = document.getElementById("page2"); // Replace "container" with the actual ID of your container element
+
+    if (!filterDropdown || !filterIcon || !container) {
+        return;
+    }
+
+    filterIcon.addEventListener("click", () => {
+        if (filterDropdown.classList.contains("filter-dropdown-open")) {
+            filterDropdown.classList.remove("filter-dropdown-open");
+            container.classList.remove("filter-blur"); // Remove blur class
+        } else {
+            filterDropdown.classList.add("filter-dropdown-open");
+            container.classList.add("filter-blur"); // Add blur class
+        }
+    });
+}
+
+dropDown();
