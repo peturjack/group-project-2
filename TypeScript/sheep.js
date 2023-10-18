225bf7ad89f061a325bfe8b57abc086668537b99
rebekkarutm <rmdottir@gmail.com>
2023-10-18 14:25:45 +0000

    tidied up final

--- TypeScript/sheep.js
+++ TypeScript/sheep.js
@@ -1,23 +1,21 @@
 "use strict";
 //sheep animation
 let walk = 0;
-function mySheep() {
-    let sheep = document.querySelector(".sheep img");
-    let pos = 0;
-    clearInterval(walk);
-    walk = setInterval(frame, 50);
-    function frame() {
-        if (pos >= 360) {
+let sheep = document.querySelector(".sheep img");
+let pos = 0;
+clearInterval(walk);
+walk = setInterval(frame, 50);
+function frame() {
+    if (pos >= 360) {
+        clearInterval(walk);
+    }
+    else {
+        if (sheep === null) {
             clearInterval(walk);
+            return;
         }
-        else {
-            if (sheep === null) {
-                clearInterval(walk);
-                return;
-            }
-            pos++;
-            sheep.style.position = "absolute";
-            sheep.style.left = pos + "rem";
-        }
+        pos++;
+        sheep.style.position = "absolute";
+        sheep.style.left = pos + "rem";
     }
 }
